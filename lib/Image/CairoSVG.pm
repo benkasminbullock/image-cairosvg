f55d56e6b37b8ff4ff9093effe7fb9aeac689c9f
Ben Bullock <benkasminbullock@gmail.com>
2014-05-18 09:10:50 +0900

    Initial commit of image-cairosvg

---
+++ lib/Image/CairoSVG.pm
@@ -0,0 +1,27 @@
+=encoding UTF-8
+
+=head1 NAME
+
+Image::CairoSVG - abstract here.
+
+=head1 SYNOPSIS
+
+    use Image::CairoSVG;
+
+=head1 DESCRIPTION
+
+=head1 FUNCTIONS
+
+=cut
+package Image::CairoSVG;
+require Exporter;
+@ISA = qw(Exporter);
+@EXPORT_OK = qw//;
+%EXPORT_TAGS = (
+    all => \@EXPORT_OK,
+);
+use warnings;
+use strict;
+use Carp;
+our $VERSION = 0.01;
+1;
---
+++ t/image-cairosvg.t
@@ -0,0 +1,11 @@
+# This is a test for module Image::CairoSVG.
+
+use warnings;
+use strict;
+use Test::More;
+use Image::CairoSVG;
+
+done_testing ();
+# Local variables:
+# mode: perl
+# End:
