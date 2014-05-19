9263044e7b463cba32bd0243cb4d6b7d0a99c4b9
Ben Bullock <benkasminbullock@gmail.com>
2014-05-19 14:11:25 +0900

    Arc-drawing code, taken from cairosvg (Python)
    The "Church" test files test drawing arcs.
    
    I've also added note on copying the arc code from the Python in the
    documentation.

--- lib/Image/CairoSVG.pm
+++ lib/Image/CairoSVG.pm
@@ -2,7 +2,7 @@
 
 =head1 NAME
 
-Image::CairoSVG - abstract here.
+Image::CairoSVG - render SVG into a Cairo surface
 
 =head1 SYNOPSIS
 
@@ -22,9 +22,23 @@ This example converts an SVG into a PNG:
 
 =head2 new
 
+    my $cairosvg = Image::CairoSVG->new ();
+
+Use default surface.
+
+    my $cairosvg = Image::CairoSVG->new (surface => $surface);
+
+With a surface
+
+Not implemented yet:
+
+    my $cairosvg = Image::CairoSVG->new (context => $cr);
+
 =head2 render
 
-Draw an SVG into a Cairo surface.
+    $cairosvg->render ('some.svg');
+
+Draw an SVG file into a Cairo surface.
 
 =head2 line
 
@@ -37,9 +51,9 @@ Cairo surface.
 
     $cairosvg->path (%attr);
 
-=head2 rectangle
+=head2 rect
 
-    $cairosvg->rectangle (%attr);
+    $cairosvg->rect (%attr);
 
 =head1 Dependencies
 
@@ -83,7 +97,8 @@ use strict;
 use Carp;
 use XML::Parser;
 use Cairo;
-use Image::SVG::Path 'extract_path_info';
+use Image::SVG::Path qw/extract_path_info create_path_string/;
+use constant M_PI => 3.14159265358979;
 our $VERSION = 0.01;
 
 our $default_surface_type = 'argb32';
@@ -103,19 +118,28 @@ dimensions 100 by 100 of rgba format.
 sub new
 {
     my ($class, %options) = @_;
-    my $surface = $options{surface};
-    if (! $surface) {
-	$surface = Cairo::ImageSurface->create (
-	    $default_surface_type,
-	    $default_surface_size,
-	    $default_surface_size,
-	);
-    }
+
     my $self = {};
-    $self->{surface} = $surface;
-    $self->{cr} = Cairo::Context->create ($self->{surface});
-    if (! $self->{cr}) {
-	die "Cairo::Context->create failed";
+
+    my $context = $options{context};
+
+    if ($context) {
+	$self->{cr} = $context;
+    }
+    else {
+	my $surface = $options{surface};
+	if (! $surface) {
+	    $surface = Cairo::ImageSurface->create (
+		$default_surface_type,
+		$default_surface_size,
+		$default_surface_size,
+	    );
+	}
+	$self->{surface} = $surface;
+	$self->{cr} = Cairo::Context->create ($self->{surface});
+	if (! $self->{cr}) {
+	    die "Cairo::Context->create failed";
+	}
     }
     return bless $self;
 }
@@ -132,6 +156,7 @@ sub render
 {
     my ($self, $file) = @_;
     die unless -f $file;
+    $self->{file} = $file;
     my $p = XML::Parser->new (
 	Handlers => {
 
@@ -142,6 +167,9 @@ sub render
 	    Start => sub {
 		handle_start ($self, @_);
 	    },
+	    End => sub {
+		handle_end ($self, @_);
+	    },
 	},
     );
     my $cr = $self->{cr};
@@ -150,6 +178,15 @@ sub render
 	die "No context in $self";
     }
     $p->parsefile ($file);
+}
+
+sub handle_end
+{
+    my ($self, $parser, $tag) = @_;
+    # At the end of a group, delete its attributes.
+    if ($tag eq 'g') {
+	delete $self->{attr};
+    }
 }
 
 # Start tag handler for the XML parser. This is private.
@@ -167,17 +204,106 @@ sub handle_start
     elsif ($tag eq 'line') {
 	$self->line (%attr);
     }
+    elsif ($tag eq 'circle') {
+	$self->circle (%attr);
+    }
+    elsif ($tag eq 'ellipse') {
+	$self->ellipse (%attr);
+    }
+    elsif ($tag eq 'rect') {
+	$self->rect (%attr);
+    }
     elsif ($tag eq 'svg' ||
 	   $tag eq 'title') {
 	;
     }
+    elsif ($tag eq 'g') {
+	$self->{attr} = \%attr;
+    }
     else {
-	warn "Unknown tag '$tag'";
+	warn "Unknown tag '$tag' in $self->{file}";
     }
 
     # http://www.princexml.com/doc/7.1/svg/
     # g, rect, circle, ellipse, line, polyline, polygon, path, text, tspan
 
+}
+
+=head2 rect
+
+    $cairosvg->rect (%attr);
+
+=cut
+
+sub rect
+{
+    my ($self, %attr) = @_;
+
+    my $x = $self->convert_svg_units ($attr{x});
+    my $y = $self->convert_svg_units ($attr{y});
+    my $width = $self->convert_svg_units ($attr{width});
+    my $height = $self->convert_svg_units ($attr{height});
+
+    my $cr = $self->{cr};
+
+    $cr->rectangle ($x, $y, $width, $height);
+
+    $self->do_svg_attr (%attr);
+}
+
+=head2 ellipse
+
+    $cairosvg->ellipse (%attr);
+
+=cut
+
+sub ellipse
+{
+    my ($self, %attr) = @_;
+
+    my $cx = $self->convert_svg_units ($attr{cx});
+    my $cy = $self->convert_svg_units ($attr{cy});
+    my $rx = $self->convert_svg_units ($attr{rx});
+    my $ry = $self->convert_svg_units ($attr{ry});
+
+    my $cr = $self->{cr};
+
+    # http://cairographics.org/manual/cairo-Paths.html#cairo-arc
+
+    $cr->save ();
+    $cr->translate ($cx, $cy);
+    $cr->scale ($rx, $ry);
+
+    # Render it.
+
+    $cr->arc (0, 0, 1, 0, 2*M_PI);
+
+    $cr->restore ();
+
+    $self->do_svg_attr (%attr);
+}
+
+=head2 circle
+
+    $cairosvg->circle (%attr);
+
+=cut
+
+sub circle
+{
+    my ($self, %attr) = @_;
+
+    my $cx = $self->convert_svg_units ($attr{cx});
+    my $cy = $self->convert_svg_units ($attr{cy});
+    my $r = $self->convert_svg_units ($attr{r});
+
+    my $cr = $self->{cr};
+
+    # Render it.
+
+    $cr->arc ($cx, $cy, $r, 0, 2*M_PI);
+
+    $self->do_svg_attr (%attr);
 }
 
 =head2 polygon
@@ -231,6 +357,8 @@ sub path
 	absolute => 1,
 	no_shortcuts => 1,
     });
+#    print create_path_string (\@path_info), "\n";
+
 
     # Cairo context.
 
@@ -265,11 +393,13 @@ sub path
 	if ($key eq 'M') {
 	    $cr->new_sub_path ();
 	    $cr->move_to (@{$element->{point}});
+#	    print "move to @{$element->{point}}\n";
 	}
 	elsif ($key eq 'L') {
 	    my $point = $element->{point};
 	    my ($x, $y) = @{$point};
 	    die unless defined $x && defined $y;
+#	    print "draw line to $x $y [@$point]\n";
 	    $cr->line_to ($x, $y);
 	}
 	elsif ($key eq 'C') {
@@ -285,11 +415,96 @@ sub path
 	    # to shim this
 	    quadbez ($cr, $element->{control}, $element->{end});
 	}
+	elsif ($key eq 'V') {
+	    my ($xo, $yo) = $cr->get_current_point ();
+	    my $y = $element->{y};
+	    die unless defined $y;
+#	    print "draw to $xo $y\n";
+	    $cr->line_to ($xo, $y);
+	}
+	elsif ($key eq 'H') {
+	    my ($xo, $yo) = $cr->get_current_point ();
+	    my $x = $element->{x};
+	    die unless defined $x;
+#	    print "draw to $x $yo\n";
+	    $cr->line_to ($x, $yo);
+	}
+	elsif ($key eq 'A') {
+	    $self->svg_arc ($element);
+	}
 	else {
-	    croak "Unknown SVG path key '$key'";
+	    warn "Unknown SVG path key '$key'";
 	}
     }
     $self->do_svg_attr (%attr);
+}
+
+# This is a perl of 
+# https://github.com/Kozea/CairoSVG/blob/74701790b5fd299e99f993b18ea676f3284907b4/cairosvg/surface/path.py
+
+sub svg_arc
+{
+    my ($self, $element) = @_;
+    my $rx = $element->{rx};
+    my $ry = $element->{ry};
+    my $x3 = $element->{x};
+    my $y3 = $element->{y};
+    my $x_axis_rotation = $element->{x_axis_rotation};
+    my $large_arc_flag = $element->{large_arc_flag};
+    my $sweep_flag = $element->{sweep_flag};
+
+#    warn "A is unsupported";
+    my $cr = $self->{cr};
+
+    # rx=0 or ry=0 means straight line
+
+    if ($rx == 0 || $ry == 0) {
+	$cr->line_to ($x3, $y3);
+	return;
+    }
+
+    my ($x1, $y1) = $cr->get_current_point ();
+
+    # Translate $x3, $y3 to relative coords
+
+    $x3 -= $x1;
+    $y3 -= $y1;
+
+    my $radii_ratio = $ry / $rx;
+
+    my ($xe, $ye) = rotate ($x3, $y3, -$x_axis_rotation);
+
+    $ye /= $radii_ratio;
+    my $angle = point_angle (0, 0, $xe, $ye);
+
+    $xe = sqrt ($xe**2 + $ye**2);
+    $ye = 0;
+
+    if ($xe / 2 > $rx) {
+	$rx = $xe / 2;
+    }
+    my $xc = $xe / 2;
+    my $yc = sqrt ($rx**2 - $xc**2);
+
+    if (! ($large_arc_flag ^ $sweep_flag)) {
+	$yc = -$yc;
+    }
+    ($xe, $ye) = rotate ($xe, 0, $angle);
+    ($xc, $yc) = rotate ($xc, $yc, $angle);
+
+    my $angle1 = point_angle ($xc, $yc, 0, 0);
+    my $angle2 = point_angle ($xc, $yc, $xe, $ye);
+    $cr->save ();
+
+    $cr->translate ($x1, $y1);
+    if ($sweep_flag) {
+	$cr->arc ($xc, $yc, $rx, $angle1, $angle2);
+    }
+    else {
+	$cr->arc_negative ($xc, $yc, $rx, $angle1, $angle2);
+    }
+    $cr->restore ();
+
 }
 
 # Quadratic bezier curve shim for Cairo
@@ -347,11 +562,22 @@ sub line
 sub convert_svg_units
 {
     my ($self, $thing) = @_;
+    if (! defined $thing) {
+#	print "Undefiend\n";
+	return 0;
+    }
+    if ($thing eq '') {
+#	print "nothing";
+	return 0;
+    }
+    if ($thing =~ /^(\d|\.)+$/) {
+	return $thing;
+    }
     if ($thing =~ /(\d+)px/) {
 	$thing =~ s/px$//;
 	return $thing;
     }
-    die "Dunno what to do with $thing";
+    die "Dunno what to do with '$thing'";
 }
 
 # We have a path in the cairo surface and now we have to do the SVG
@@ -360,7 +586,19 @@ sub convert_svg_units
 sub do_svg_attr
 {
     my ($self, %attr) = @_;
-    confess "Nothing to do" unless keys %attr > 0;
+
+    # Copy attributes from "self".
+
+    if ($self->{attr}) {
+	for my $k (keys %{$self->{attr}}) {
+	    if (! $attr{$k}) {
+		$attr{$k} = $self->{attr}{$k};
+	    }
+	}
+    }
+
+
+#    confess "Nothing to do" unless keys %attr > 0;
 
     my $fill = $attr{fill};
     my $stroke = $attr{stroke};
@@ -372,23 +610,63 @@ sub do_svg_attr
     }
     if ($fill && $fill ne 'none') {
 	if ($stroke && $stroke ne 'none') {
+	    $self->set_colour ($fill);
 	    $cr->fill_preserve ();
+	    $self->set_colour ($stroke);
 	    $cr->stroke ();
 	}
 	else {
+	    $self->set_colour ($fill);
 	    $cr->fill ();
 	}
     }
     elsif ($stroke && $stroke ne 'none') {
+	$self->set_colour ($stroke);
 	$cr->stroke ();
     }
 }
 
+sub set_colour
+{
+    my ($self, $colour) = @_;
+    my $cr = $self->{cr};
+    # Hex digit
+    my $h = qr/[0-9a-f]/i;
+    if ($colour eq 'black') {
+	$cr->set_source_rgb (0, 0, 0);
+    }
+    elsif ($colour eq 'white') {
+	$cr->set_source_rgb (1, 1, 1);
+    }
+    elsif ($colour =~ /^#($h)($h)($h)$/) {
+	$cr->set_source_rgb (hex ($1), hex ($2), hex ($3));
+    }
+    else {
+	warn "Unknown colour '$colour'";
+    }
+}
 
 sub surface
 {
     my ($self) = @_;
     return $self->{surface};
+}
+
+# Return angle between x axis and point knowing given center.
+
+# https://github.com/Kozea/CairoSVG/blob/74701790b5fd299e99f993b18ea676f3284907b4/cairosvg/surface/helpers.py#L116
+
+sub point_angle
+{
+    my ($cx, $cy, $px, $py) = @_;
+    return atan2 ($py - $cy, $px - $cx);
+}
+
+sub rotate
+{
+    my ($x, $y, $angle) = @_;
+    return ($x * cos ($angle) - $y * sin ($angle),
+	    $y * cos ($angle) + $x * sin ($angle));
 }
 
 1;
@@ -404,5 +682,12 @@ contribute your addition to this module via git.
 =head1 LICENCE, COPYRIGHT, AUTHOR
 
 Copyright 2014 Ben Bullock bkb@cpan.org. Licence = Perl (Artistic + GNU).
+
+Some parts of the module (specifically the SVG arc drawing code) are
+translations from the above-mentioned Python program "cairosvg", which
+is under the "GNU LESSER GENERAL PUBLIC LICENSE Version 3, 29 June
+2007". I'm not really sure how or if this affects the code, but just
+in case it causes legal issues for someone downstream, I'm mentioning
+it here.
 
 =cut
--- t/image-cairosvg.t
+++ t/image-cairosvg.t
@@ -16,22 +16,30 @@ my $cairosvg2 = Image::CairoSVG->new (
 );
 ok ($cairosvg2);
 
-my $stem = "$FindBin::Bin/Technical_college";
-my $file = "$stem.svg";
-my $testpng = "$stem.png";
 
-$cairosvg2->render ($file);
-my $tempout = "$FindBin::Bin/TC-out.png";
-$surface->write_to_png ($tempout);
 SKIP: {
     eval {
 	require Image::PNG::Libpng;
     };
-    skip "No Image::PNG::Libpng", 1 if $@;
-    my $diff = Image::PNG::Libpng::image_data_diff ($testpng, $tempout);
-    ok (! $diff, "PNG files contain the same data");
-};
-unlink $tempout;
+    skip "No Image::PNG::Libpng", 2 if $@;
+
+    for my $f (qw/Technical_college Church/) {
+	my $surface = Cairo::ImageSurface->create ('argb32', 400, 400);
+	my $cairosvg2 = Image::CairoSVG->new (
+	    surface => $surface,
+	);
+	my $stem = "$FindBin::Bin/$f";
+	my $file = "$stem.svg";
+	my $testpng = "$stem.png";
+	die unless -f $testpng;
+	$cairosvg2->render ($file);
+	my $tempout = "$stem-out.png";
+	$surface->write_to_png ($tempout);
+	my $diff = Image::PNG::Libpng::image_data_diff ($testpng, $tempout);
+	ok (! $diff, "PNG files contain the same data");
+	unlink $tempout;
+    };
+}
 
 done_testing ();
 
